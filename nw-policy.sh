e6fe9c1cb1339250ca70cb342be19f630ebc3699
sat0ken <15720506+sat0ken@users.noreply.github.com>
2024-04-04 19:13:52 +0900

    update

--- nw-policy.sh
+++ nw-policy.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 
+set_ingress_nw_policy_peer() {
+	spec=$1
+	nw_policy_peers=$2
+	for peer in "${nw_policy_peers[@]}"
+	do
+		if [ $peer = "ipBlock" ]; then
+			echo "ipBlock";
+		elif [ $peer = "namespaceSelector" ]; then
+			echo "namespaceSelector";
+		else
+			echo "podSelector!!!";
+		fi
+	done
+}
+
+set_ingress() {
+	ingress_list=($(echo "$1" | jq -r '.spec.ingress[] | keys | .[]'))
+
+	for ingress in "${ingress_list[@]}"
+	do
+		if [ $ingress = "from" ]; then
+			from=($(echo "$1" | jq -r .spec.ingress[] | jq -r '.from[] | keys | .[]'))
+			set_ingress_nw_policy_peer $1 $from
+		else
+			# Todo: portsの処理
+			:
+		fi
+	done
+}
+
 kubectl get networkpolicies.networking.k8s.io -A -o json | jq -c .items[] | while read -r item; do
     name=$(echo "$item" | jq -r .metadata.name)
 	namespace=$(echo "$item" | jq -r .metadata.namespace)
 	policy_types=($(echo "$item" | jq -r .spec.policyTypes[]))
 	pod_labels=$(echo "$item" | jq -c '.spec.podSelector.matchLabels' | sed -e "s/{//" -e "s/}//" -e "s/\"//g" -e "s/:/=/")
 	pod_ips=($(kubectl -n $namespace get pod -l $pod_labels -o json | jq -r .items[].status.podIP))
+
+	set_ingress $item
 
 	ipset_name=$(printf "%s-%s" $namespace $name)
 	# ipsetが存在していなければ作成する
